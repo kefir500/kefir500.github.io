1d74682b8d89fa3385adc08b224ba7c27544ee9b
kefir500 <kefir500@gmail.com>
2014-04-13 23:42:00 +0600

    Add OneDrive redirect page

---
+++ apk-icon-editor/onedrive/onedrive.js
@@ -0,0 +1,7 @@
+function getURLParameter(name) {
+	return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null
+}
+
+function getCode() {
+	return getURLParameter('code');
+}
